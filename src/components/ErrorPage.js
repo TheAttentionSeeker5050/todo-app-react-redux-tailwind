4902bc6ce6dba39d68ef0e43a83f2197fa908ebc
nicolas castellano <nicolas.castellano.6@gmail.com>
2023-01-31 21:10:53 -0400

    modified error page

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,6 +1,20 @@
+// import fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import "@fortawesome/fontawesome-free/js/solid"
+
+// import error hooks
+import { useRouteError } from "react-router-dom";
+
 export default function ErrorPage() {
-    return (<>
+
+    const error = useRouteError()
+
+    return (
         
-        <>Error: Something happened</>
-    </>)
+        <div className="flex flex-col flex-nowrap gap-12 text-center my-20">
+            <FontAwesomeIcon icon=" fa-triangle-exclamation" size="4x" color="rgb(250 204 21)"/>
+            <h2 className="text-2xl font-semibold">Oops! some error occurred:</h2>
+            <p>{error.statusText || error.message}</p>
+        </div>
+    )
 }
