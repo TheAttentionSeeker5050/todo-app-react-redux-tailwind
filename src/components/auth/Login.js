3d83cdc6e11baedade370a852e0fffea6b4c9007
nicolas castellano <nicolas.castellano.6@gmail.com>
2023-02-14 15:27:26 -0400

    .

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,6 +1,6 @@
 import { loginAPIRequest } from "../../api/login.api";
 import { useState } from "react";
-
+import { redirect } from "react-router-dom";
 
 export default function LoginPage() {
 
@@ -10,17 +10,23 @@ export default function LoginPage() {
 
     // handlesubmit
     const handleSubmit = (e) => {
-        const response = loginAPIRequest(email, password);
+
+        let response = loginAPIRequest(email, password);
+
         response.
             then(data => {
                 window.localStorage.setItem("webToken", data.token);
                 window.localStorage.setItem("isLoggedIn", true);
-                window.location.href = "/"
+                console.log("request-success");
             })
+
             .catch(error => {
                 window.localStorage.removeItem("webToken");
                 window.localStorage.setItem("isLoggedIn", false);
+                console.log("request-fail");
+
             })
+            
     }
 
     const handleFormInput = (e) => {
@@ -32,38 +38,45 @@ export default function LoginPage() {
             setPassword(e.target.value);
         }
     }
-    
-    return (
-        <main className="w-screen flex flex-col gap-5 flex-nowrap text-center content-evenly my-24">
+    // console.log(window.localStorage.getItem("isLoggedIn"))
+    // check if user is logged in to redirect to todos
+    if (window.localStorage.getItem("isLoggedIn")) {
+        window.location.href = "/todos";
 
-            <h1 className="text-2xl">
-                Login
-            </h1>
+    } else {
+        
+        return (
+            <main className="w-screen flex flex-col gap-5 flex-nowrap text-center content-evenly my-24">
 
-            <section className="">
-                <form className="flex flex-col gap-4 justify-between" onSubmit={(e) => handleSubmit(e)}>
+                <h1 className="text-2xl">
+                    Login
+                </h1>
 
-                    <div className="flex flex-col w-60 mx-auto justify-between">
-                        <label className="text-left">Email:</label>
-                        <input type="email" name="email" value={email} autoComplete="email" onChange={(e) => handleFormInput(e)} className="border-2 border-sky-700 rounded-lg"></input>
-                    </div>
+                <section className="">
+                    <form className="flex flex-col gap-4 justify-between" onSubmit={(e) => handleSubmit(e)}>
 
-                    <div className="flex flex-col w-60 mx-auto justify-between">
-                        <label className="text-left">Password:</label> 
-                        <input type="password" name="password" autoComplete="password" value={password} onChange={(e) => handleFormInput(e)} className="border-2 border-sky-700 rounded-lg"></input>
-                    </div>
+                        <div className="flex flex-col w-60 mx-auto justify-between">
+                            <label className="text-left">Email:</label>
+                            <input type="email" name="email" value={email} autoComplete="email" onChange={(e) => handleFormInput(e)} className="border-2 border-sky-700 rounded-lg"></input>
+                        </div>
 
-                    <div >
-                        <button type="submit"
-                            className="bg-sky-700 w-32 p-3 text-white font-semibold rounded-xl my-12"
-                            >
-                            Submit
-                        </button>
-                    </div>
+                        <div className="flex flex-col w-60 mx-auto justify-between">
+                            <label className="text-left">Password:</label> 
+                            <input type="password" name="password" autoComplete="password" value={password} onChange={(e) => handleFormInput(e)} className="border-2 border-sky-700 rounded-lg"></input>
+                        </div>
 
-                </form>
-            </section>
+                        <div >
+                            <button type="submit"
+                                className="bg-sky-700 w-32 p-3 text-white font-semibold rounded-xl my-12"
+                                >
+                                Submit
+                            </button>
+                        </div>
+
+                    </form>
+                </section>
             
-        </main>
-    )
+            </main>
+        )
+    }
 }
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,53 +1,62 @@
+
+import { useState } from "react";
+
 export default function RegisterPage() {
 
-    
-    return (
-        <main className="w-screen flex flex-col gap-5 flex-nowrap text-center content-evenly my-24">
+    if (window.localStorage.getItem("isLoggedIn")) {
+        window.location.href = "/todos";
 
-            <h1 className="text-2xl">
-                Register
-            </h1>
+    } else {
 
-            <section className="">
-                <form className="flex flex-col gap-4 justify-between " >
-                    <div className="flex flex-col w-60 mx-auto justify-between">
-                        <label className="text-left">First Name:</label>
-                        <input type="text" name="firstName" className="border-2 border-sky-700 rounded-lg"></input>
-                    </div>
+        
+        return (
+            <main className="w-screen flex flex-col gap-5 flex-nowrap text-center content-evenly my-24">
 
-                    <div className="flex flex-col w-60 mx-auto justify-between">
-                        <label className="text-left">Last Name: </label>
-                        <input type="text" name="lastName" className="border-2 border-sky-700 rounded-lg"></input>
-                    </div>
+                <h1 className="text-2xl">
+                    Register
+                </h1>
 
-                    <div className="flex flex-col w-60 mx-auto justify-between">
-                        <label className="text-left">Email:</label>
-                        <input type="email" name="email" className="border-2 border-sky-700 rounded-lg"></input>
-                    </div>
+                <section className="">
+                    <form className="flex flex-col gap-4 justify-between " >
+                        <div className="flex flex-col w-60 mx-auto justify-between">
+                            <label className="text-left">First Name:</label>
+                            <input type="text" name="firstName" className="border-2 border-sky-700 rounded-lg"></input>
+                        </div>
 
-                    <div className="flex flex-col w-60 mx-auto justify-between">
-                        <label className="text-left">Password:</label> 
-                        <input type="password" name="password" className="border-2 border-sky-700 rounded-lg"></input>
-                    </div>
+                        <div className="flex flex-col w-60 mx-auto justify-between">
+                            <label className="text-left">Last Name: </label>
+                            <input type="text" name="lastName" className="border-2 border-sky-700 rounded-lg"></input>
+                        </div>
 
-                    <div className="flex flex-col w-60 mx-auto justify-between gap-2">
-                        <label className="text-left">Password Confirmation:</label> 
-                        <input type="password" name="passwordConfirmation" className="border-2 border-sky-700 rounded-lg"></input>
-                    </div>
+                        <div className="flex flex-col w-60 mx-auto justify-between">
+                            <label className="text-left">Email:</label>
+                            <input type="email" name="email" className="border-2 border-sky-700 rounded-lg"></input>
+                        </div>
 
-                    <div >
-                        <button type="submit"
-                            className="bg-sky-700 w-32 p-3 text-white font-semibold rounded-xl my-12"
-                            >
-                            Submit
-                        </button>
-                    </div>
+                        <div className="flex flex-col w-60 mx-auto justify-between">
+                            <label className="text-left">Password:</label> 
+                            <input type="password" name="password" className="border-2 border-sky-700 rounded-lg"></input>
+                        </div>
 
-                </form>
-            </section>
+                        <div className="flex flex-col w-60 mx-auto justify-between gap-2">
+                            <label className="text-left">Password Confirmation:</label> 
+                            <input type="password" name="passwordConfirmation" className="border-2 border-sky-700 rounded-lg"></input>
+                        </div>
 
-            
-            
-        </main>
-    )
+                        <div >
+                            <button type="submit"
+                                className="bg-sky-700 w-32 p-3 text-white font-semibold rounded-xl my-12"
+                                >
+                                Submit
+                            </button>
+                        </div>
+
+                    </form>
+                </section>
+
+                
+                
+            </main>
+        )
+    }
 }
